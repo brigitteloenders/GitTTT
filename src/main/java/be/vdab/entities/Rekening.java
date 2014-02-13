4ae214e1fe34095feff6e1fc416bd546868ef3d9
Brigitte Loenders <brigitte.loenders@vdab.be>
2014-02-13 10:39:12 +0100

    saldo variabele is geïnitialiseerd

--- src/main/java/be/vdab/entities/Rekening.java
+++ src/main/java/be/vdab/entities/Rekening.java
@@ -3,7 +3,7 @@ package be.vdab.entities;
 import java.math.BigDecimal;
 
 public class Rekening {
-	private BigDecimal saldo;
+	private BigDecimal saldo=BigDecimal.ZERO;
 
 	public BigDecimal getSaldo() {
 		return saldo;
