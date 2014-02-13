bd54b2c10614221219b4f7811806e6cfeb5a876f
Brigitte Loenders <brigitte.loenders@vdab.be>
2014-02-13 11:41:47 +0100

    Bij servlet Valentijn een import van FileReader toegevoegd

--- src/main/java/be/vdab/servlets/Valentijn.java
+++ src/main/java/be/vdab/servlets/Valentijn.java
@@ -1,6 +1,8 @@
 package be.vdab.servlets;
 
+import java.io.FileReader;
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
