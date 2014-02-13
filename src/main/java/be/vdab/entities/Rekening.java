769d1378ef0cd75d2a7cf27bb1bb4650857953a3
Brigitte Loenders <brigitte.loenders@vdab.be>
2014-02-13 11:52:47 +0100

    met javadoc

--- src/main/java/be/vdab/entities/Rekening.java
+++ src/main/java/be/vdab/entities/Rekening.java
@@ -2,9 +2,18 @@ package be.vdab.entities;
 
 import java.math.BigDecimal;
 
+/** 
+ * Een rekening van een bank.
+ * @author bloender
+ *
+ */
 public class Rekening {
 	private BigDecimal saldo=BigDecimal.ZERO;
 
+	/**
+	 * Het saldo ophalen.
+	 * @return het saldo (BigDecimal)
+	 */
 	public BigDecimal getSaldo() {
 		return saldo;
 	}
